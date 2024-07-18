b0f702bcbd1beeb28f03da6e4853a9ea485ebde7
vvalntnv <vvalntnv@gmail.com>
2024-07-18 03:53:21 -0400

    Rust application created

--- audio_book_api/src/main.rs
+++ audio_book_api/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{self, web, App, HttpResponse, HttpServer};
+
+async fn healthpoint() -> HttpResponse {
+    HttpResponse::Ok().body("I am ALIVE!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(healthpoint))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await 
 }
