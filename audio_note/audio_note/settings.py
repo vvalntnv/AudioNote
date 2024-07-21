3127589206435d57f4f65d2c0b13f59f732b8069
Viktor Valentinov <vvalntnv@gmail.com>
2024-07-21 10:45:00 +0300

    installed tailwind

--- audio_note/audio_note/settings.py
+++ audio_note/audio_note/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # External apps
+    'tailwind',
+
+    # Internal apps
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Tailwind config
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
